806b1248511e4e7a7bade224194344ef3cc6aa25
afilipovski <filipovskia02@gmail.com>
2024-04-29 09:00:01 +0200

    Add project files.

---
+++ EShop.Repository/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using EShop.Domain.Domain;
+using EShop.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Repository
+{
+    public class ApplicationDbContext : IdentityDbContext<EShopApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Ticket> Products { get; set; }
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<TicketInShoppingCart> ProductInShoppingCarts { get; set; }
+        public virtual DbSet<TicketInOrder> ProductInOrders { get; set; }
+    }
+}
---
+++ EShop.Repository/Implementation/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using EShop.Domain.Domain;
+using EShop.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Repository.Implementation
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
